b013b34b435840bdcea6e74544b7311091e01917
mmorrison6 <62436842+mmorrison6@users.noreply.github.com>
2023-03-19 22:29:04 -0500

    Added Videos to the Players' Press Page

--- Website/src/components/Video.jsx
+++ Website/src/components/Video.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import 'vidstack/styles/defaults.css';
 import { MediaOutlet, MediaPlayer } from '@vidstack/react';
 
-
-export default function Counter() {
+export default function Video({source, poster}) {
+  const [autoplay, loop] = "true";
   return <MediaPlayer
-  src="https://gateway.pinata.cloud/ipfs/Qme47jcBrZBWdgrRijag587zxfURdoFsM7oa3G4yggiM5r?_gl=1*s7wbop*_ga*NGE1NmQ4NzgtODZiOS00NmRjLThjOTgtMGVkZWUzYmNjMjk1*_ga_5RMPXG14TE*MTY3OTA2ODg0Ni4xLjEuMTY3OTA2OTA3OC40NS4wLjA."
-  poster="https://media-files.vidstack.io/poster.png"
+  autoplay={autoplay}
+  loop={loop}
+  src={source}
+  poster={poster}
   controls
 >
   {/* ^ remove `controls` attribute if you're designing a custom UI */}
